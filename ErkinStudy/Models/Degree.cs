cca713bdb1f2614b7591860a93b9b5c7f42b0fa0
fatpenguino <aidarfeisal@gmail.com>
2019-10-09 23:23:55 +0600

    Add project files.

---
+++ ErkinStudy/Models/Degree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErkinStudy.Models
+{
+    public class Subject
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
