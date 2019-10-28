c23eefa96d581c117ea76327273d21b9d70c3b0f
Feisal Aidar <Feisal.Aidar@airastana.com>
2019-10-28 16:55:51 +0600

    lesson crud

---
+++ ErkinStudy.Web/Controllers/Admin/ContentController.cs
@@ -0,0 +1,159 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ErkinStudy.Domain.Models;
+using ErkinStudy.Infrastructure.Context;
+
+namespace ErkinStudy.Web.Controllers.Admin
+{
+    public class ContentController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Content
+        public IActionResult Index(long? lessonId)
+        {
+	        ViewBag.LessonId = lessonId;
+            return lessonId.HasValue ? View(_context.Contents.Include(x => x.Lesson).Where(x => x.LessonId == lessonId).AsQueryable()) 
+									 : View(_context.Contents.Include(x => x.LessonId == lessonId).AsQueryable());
+        }
+
+        // GET: Content/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents
+                .Include(c => c.Lesson)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // GET: Content/Create
+        public IActionResult Create(long lessonId)
+        {
+	        ViewBag.LessonId = lessonId;
+            return View();
+        }
+
+        // POST: Content/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LessonId,Value,Order,ContentFormat")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(content);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LessonId"] = new SelectList(_context.Lessons, "Id", "Id", content.LessonId);
+            return View(content);
+        }
+
+        // GET: Content/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents.FindAsync(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+            ViewData["LessonId"] = new SelectList(_context.Lessons, "Id", "Id", content.LessonId);
+            return View(content);
+        }
+
+        // POST: Content/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,LessonId,Value,Order,ContentFormat")] Content content)
+        {
+            if (id != content.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(content);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContentExists(content.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LessonId"] = new SelectList(_context.Lessons, "Id", "Id", content.LessonId);
+            return View(content);
+        }
+
+        // GET: Content/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents
+                .Include(c => c.Lesson)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // POST: Content/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var content = await _context.Contents.FindAsync(id);
+            _context.Contents.Remove(content);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContentExists(long id)
+        {
+            return _context.Contents.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ErkinStudy.Web/Controllers/Admin/LessonController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ErkinStudy.Domain.Models;
+using ErkinStudy.Infrastructure.Context;
+
+namespace ErkinStudy.Web.Controllers.Admin
+{
+    public class LessonController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public LessonController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lesson
+        public IActionResult Index(long? paragraphId)
+        {
+	        ViewBag.ParagraphId = paragraphId;
+            return paragraphId.HasValue ? View(_context.Lessons.Include(l => l.Paragraph).Where(x => x.ParagraphId == paragraphId).AsQueryable()) 
+										: View(_context.Lessons.Include(x => x.Paragraph).AsQueryable());
+        }
+
+        // GET: Lesson/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lesson = await _context.Lessons
+                .Include(l => l.Paragraph)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+
+            return View(lesson);
+        }
+
+        // GET: Lesson/Create
+        public IActionResult Create(long paragraphId)
+        {
+	        ViewBag.ParagraphId = paragraphId;
+            return View();
+        }
+
+        // POST: Lesson/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ParagraphId,Name,Description,Order,Price")] Lesson lesson)
+        {
+            if (ModelState.IsValid)
+            {
+				lesson.CreatedAt = DateTime.Now;
+				_context.Add(lesson);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index), new { paragraphId = lesson.ParagraphId });
+            }
+            return View(lesson);
+        }
+
+        // GET: Lesson/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lesson = await _context.Lessons.FindAsync(id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+            return View(lesson);
+        }
+
+        // POST: Lesson/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,ParagraphId,Name,Description,Order,Price")] Lesson lesson)
+        {
+            if (id != lesson.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lesson);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+	                if (!LessonExists(lesson.Id))
+                    {
+                        return NotFound();
+                    }
+
+	                throw;
+                }
+                return RedirectToAction(nameof(Index), new { paragraphId = lesson.ParagraphId });
+            }
+            return View(lesson);
+        }
+
+        // GET: Lesson/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lesson = await _context.Lessons
+                .Include(l => l.Paragraph)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+
+            return View(lesson);
+        }
+
+        // POST: Lesson/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var lesson = await _context.Lessons.FindAsync(id);
+            var paragraphId = lesson.ParagraphId;
+            _context.Lessons.Remove(lesson);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index), new { paragraphId });
+        }
+
+        private bool LessonExists(long id)
+        {
+            return _context.Lessons.Any(e => e.Id == id);
+        }
+    }
+}
