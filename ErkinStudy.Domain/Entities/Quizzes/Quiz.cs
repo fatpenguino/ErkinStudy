c251f68daddabf32afa3a88f3470123acb9a629d
suleimanov <yerbolsbox@gmail.com>
2020-02-02 23:08:44 +0600

    Initial approve quiz

--- ErkinStudy.Domain/Entities/Quizzes/Quiz.cs
+++ ErkinStudy.Domain/Entities/Quizzes/Quiz.cs
@@ -7,6 +7,7 @@ namespace ErkinStudy.Domain.Entities.Quizzes
         public long Id { get; set; }
         public int? CategoryId { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public bool IsActive { get; set; }
         public int Order { get; set; }
         public int Price { get; set; }
