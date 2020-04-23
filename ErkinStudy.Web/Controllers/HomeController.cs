c6637076d77b99d6ee69ca7fdf91a381bc474e80
Feisal <Feisal.Aidar@kaspi.kz>
2020-04-23 21:00:35 +0600

    offer and privacy

--- ErkinStudy.Web/Controllers/HomeController.cs
+++ ErkinStudy.Web/Controllers/HomeController.cs
@@ -81,6 +81,10 @@ namespace ErkinStudy.Web.Controllers
             return View();
         }
 
+        public IActionResult Offer()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
