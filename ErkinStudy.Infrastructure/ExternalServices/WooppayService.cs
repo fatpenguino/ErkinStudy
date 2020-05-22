5ad94af23678720deecb95ef386792d5d0cd6bba
Feisal <Feisal.Aidar@kaspi.kz>
2020-05-22 19:59:32 +0600

    payment fuck

--- ErkinStudy.Infrastructure/ExternalServices/WooppayService.cs
+++ ErkinStudy.Infrastructure/ExternalServices/WooppayService.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
+using ErkinStudy.Infrastructure.DTOs;
 using WooppayService;
 
 namespace ErkinStudy.Infrastructure.ExternalServices
 {
-    public class WooppayService
+    public class WooppayPaymentService
     {
         public XmlControllerPortTypeClient CreateClient()
         {
@@ -20,6 +21,33 @@ namespace ErkinStudy.Infrastructure.ExternalServices
                 var request = new CoreLoginRequest {username = "test_merch", password = "A12345678a"};
                 var response = await client.core_loginAsync(request);
                 return response.response.session;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task Payment(PaymentRequestDto paymentRequestDto)
+        {
+            try
+            {
+                var client = CreateClient();
+                var request = new CashCreateInvoiceExtended2Request();
+                request.cardForbidden = 0;
+                request.userEmail = paymentRequestDto.Email;
+                request.userPhone = paymentRequestDto.PhoneNumber;
+                request.backUrl = "https://localhost:44379/Home";
+                request.requestUrl = $"https://localhost:44379/Payment/ConfirmOrder/{paymentRequestDto.Id}";
+                request.addInfo = "";
+                request.amount = (float) paymentRequestDto.Amount;
+                request.deathDate = DateTime.Now.AddMinutes(15).ToString("yyyy-MM-dd hh:mm:ss");
+                request.serviceType = 4;
+                request.description = "";
+                request.referenceId = paymentRequestDto.Id.ToString();
+                var response = await client.cash_createInvoice2ExtendedAsync(request);
+                
             }
             catch (Exception e)
             {
--- ErkinStudy.Web/Controllers/PaymentController.cs
+++ ErkinStudy.Web/Controllers/PaymentController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using ErkinStudy.Domain.Entities.Identity;
 using ErkinStudy.Domain.Entities.Payment;
 using ErkinStudy.Infrastructure.Context;
+using ErkinStudy.Infrastructure.ExternalServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -17,11 +18,13 @@ namespace ErkinStudy.Web.Controllers
         private readonly AppDbContext _dbContext;
         private readonly ILogger<PaymentController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
-        public PaymentController(AppDbContext dbContext, ILogger<PaymentController> logger, UserManager<ApplicationUser> userManager)
+        private readonly WooppayPaymentService _wooppayPaymentService;
+        public PaymentController(AppDbContext dbContext, ILogger<PaymentController> logger, UserManager<ApplicationUser> userManager, WooppayPaymentService wooppayPaymentService)
         {
             _dbContext = dbContext;
             _logger = logger;
             _userManager = userManager;
+            _wooppayPaymentService = wooppayPaymentService;
         }
 
         [Authorize]
@@ -31,7 +34,9 @@ namespace ErkinStudy.Web.Controllers
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
             if (folder != null && user != null)
             {
-                var existingOrder = _dbContext.Orders.FirstOrDefaultAsync(x => x.FolderId == folder.Id && x.UserId == user.Id);
+                var existingOrder = await _dbContext.Orders.FirstOrDefaultAsync(x => x.FolderId == folder.Id && x.UserId == user.Id);
+                if (existingOrder != null)
+                    return View(existingOrder);
                 var order = new Order
                 {
                     FolderId = folder.Id,
@@ -41,10 +46,18 @@ namespace ErkinStudy.Web.Controllers
                     PhoneNumber = user.PhoneNumber,
                     CreatedTime = DateTime.Now
                 };
-                await _dbContext.Orders.AddAsync(order);
+                _dbContext.Orders.Add(order);
+                _dbContext.SaveChanges();
                 var orderId = order.Id;
                 return View(order);
             }
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> MakePayment(long orderId)
+        {
+
             return View();
         }
     }
