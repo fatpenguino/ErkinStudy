976644fe14265dc723b6498c488ee896833a91c8
fatpenguino <aidarfeisal@gmail.com>
2020-12-13 03:27:33 +0600

    ubthub init

---
+++ ErkinStudy.Domain/Entities/UbtHub/Subject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ErkinStudy.Domain.Entities.UbtHub
+{
+    public class Subject
+    {
+        public short Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<SpecialtySubject> SpecialtySubjects { get; set; }
+    }
+}
---
+++ ErkinStudy.Domain/Entities/UbtHub/University.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ErkinStudy.Domain.Entities.UbtHub
+{
+    public class University
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string About { get; set; }
+        public ICollection<UniversitySpecialty> UniversitySpecialties { get; set; }
+    }
+}
