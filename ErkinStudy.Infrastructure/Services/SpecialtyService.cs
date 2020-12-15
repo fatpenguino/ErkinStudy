e7ebdf452427968a1b443e998f2a50d56c1a5964
fatpenguino <aidarfeisal@gmail.com>
2020-12-16 03:10:38 +0600

    relations between entities

--- ErkinStudy.Infrastructure/Services/SpecialtyService.cs
+++ ErkinStudy.Infrastructure/Services/SpecialtyService.cs
@@ -1,19 +1,42 @@
-﻿using ErkinStudy.Infrastructure.Context;
-using Microsoft.Extensions.Logging;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ErkinStudy.Domain.Entities.UbtHub;
+using ErkinStudy.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace ErkinStudy.Infrastructure.Services
 {
     public class SpecialtyService
     {
         private readonly AppDbContext _context;
-        private readonly ILogger<SpecialtyService> _logger;
 
-        public SpecialtyService(AppDbContext context, ILogger<SpecialtyService> logger)
+        public SpecialtyService(AppDbContext context)
         {
             _context = context;
-            _logger = logger;
         }
 
-        
+        public async Task<List<Subject>> GetSubjects(int id)
+        {
+            return await _context.SpecialtySubjects.Include(x => x.Subject).Where(x => x.SpecialtyId == id)
+                .Select(x => x.Subject).ToListAsync();
+        }
+
+        public async Task<List<Subject>> GetExcludedSubjects(int id)
+        {
+            var included = await GetSubjects(id);
+            return await _context.Subjects.Where(x => !included.Contains(x)).ToListAsync();
+        }
+
+        public async Task<List<University>> GetUniversities(int id)
+        {
+            return await _context.UniversitySpecialties.Include(x => x.University).Where(x => x.SpecialtyId == id)
+                .Select(x => x.University).ToListAsync();
+        }
+        public async Task<List<University>> GetExcludedUniversities(int id)
+        {
+            var included = await GetUniversities(id);
+            return await _context.Universities.Where(x => !included.Contains(x)).ToListAsync();
+        }
     }
 }
--- ErkinStudy.Web/Controllers/Admin/SpecialtyController.cs
+++ ErkinStudy.Web/Controllers/Admin/SpecialtyController.cs
@@ -1,19 +1,24 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ErkinStudy.Domain.Entities.UbtHub;
 using ErkinStudy.Infrastructure.Context;
+using ErkinStudy.Web.Models.UbtHub;
+using Microsoft.Extensions.Logging;
 
 namespace ErkinStudy.Web.Controllers.Admin
 {
     public class SpecialtyController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly ILogger<SpecialtyController> _logger;
 
-        public SpecialtyController(AppDbContext context)
+        public SpecialtyController(AppDbContext context, ILogger<SpecialtyController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         // GET: Specialty
@@ -129,10 +134,116 @@ namespace ErkinStudy.Web.Controllers.Admin
             return _context.Specialties.Any(e => e.Id == id);
         }
 
+        #region Subjects
         public async Task<IActionResult> ManageSubjects(int id)
         {
-            var specialtySubjects = await _context.SpecialtySubjects.Where(x => x.SpecialtyId == id).ToListAsync();
-            return View(specialtySubjects);
+            var specialty = await _context.Specialties.FirstOrDefaultAsync(x => x.Id == id);
+            if (specialty == null)
+                return RedirectToAction("Index");
+
+            return View(specialty);
         }
+
+        public async Task<JsonResult> AddSubjects([FromBody] AddSpecialtyItemsViewModel model)
+        {
+            try
+            {
+                var specialty = await _context.Specialties.FirstOrDefaultAsync(x => x.Id == model.SpecialtyId);
+                if (specialty == null)
+                    return Json(0);
+                foreach (var subject in model.CheckedItems)
+                {
+                    await _context.SpecialtySubjects.AddAsync(new SpecialtySubject()
+                    { SpecialtyId = specialty.Id, SubjectId = short.Parse(subject) });
+                }
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Ошибка при добавлений предметов {e}");
+            }
+            return Json(0);
+        }
+
+        public async Task<IActionResult> DeleteSubject(int specialtyId, short subjectId)
+        {
+            try
+            {
+                var subjectSpecialty =
+                  await _context.SpecialtySubjects.FirstOrDefaultAsync(x =>
+                        x.SpecialtyId == specialtyId && x.SubjectId == subjectId);
+                if (subjectSpecialty == null)
+                {
+                    _logger.LogError($"Нету такой subjectSpecialty specId:{specialtyId}, subjId {subjectId}");
+                    return RedirectToAction("ManageSubjects", new { id = specialtyId });
+                }
+                _context.SpecialtySubjects.Remove(subjectSpecialty);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Ошибка при удалений subjectSpecialty  specId:{specialtyId}, subjId {subjectId}, {e}");
+            }
+            return RedirectToAction("ManageSubjects", new { id = specialtyId });
+        }
+
+        #endregion
+
+
+        #region Universities
+        public async Task<IActionResult> ManageUniversities(int id)
+        {
+            var specialty = await _context.Specialties.FirstOrDefaultAsync(x => x.Id == id);
+            if (specialty == null)
+                return RedirectToAction("Index");
+
+            return View(specialty);
+        }
+
+        public async Task<JsonResult> AddUniversities([FromBody] AddSpecialtyItemsViewModel model)
+        {
+            try
+            {
+                var specialty = await _context.Specialties.FirstOrDefaultAsync(x => x.Id == model.SpecialtyId);
+                if (specialty == null)
+                    return Json(0);
+                foreach (var item in model.CheckedItems)
+                {
+                    await _context.UniversitySpecialties.AddAsync(new UniversitySpecialty()
+                    { SpecialtyId = specialty.Id, UniversityId = int.Parse(item) });
+                }
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Ошибка при добавлений UniversitySpecialties {e}");
+            }
+            return Json(0);
+        }
+
+        public async Task<IActionResult> DeleteUniversity(int specialtyId, short universityId)
+        {
+            try
+            {
+                var universitySpecialty =
+                  await _context.UniversitySpecialties.FirstOrDefaultAsync(x =>
+                        x.SpecialtyId == specialtyId && x.UniversityId == universityId);
+                if (universitySpecialty == null)
+                {
+                    _logger.LogError($"Нету такой universitySpecialty specId:{specialtyId}, univerId {universityId}");
+                    return RedirectToAction("ManageUniversities", new { id = specialtyId });
+                }
+                _context.UniversitySpecialties.Remove(universitySpecialty);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Ошибка при удалений universitySpecialty  specId:{specialtyId}, univerId {universityId}, {e}");
+            }
+            return RedirectToAction("ManageUniversities", new { id = specialtyId });
+        }
+
+
+        #endregion
     }
 }
