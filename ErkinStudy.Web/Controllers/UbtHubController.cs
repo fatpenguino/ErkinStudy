7a5aa1110e6767a3f060c9cb3121bc37f524741b
fatpenguino <aidarfeisal@gmail.com>
2021-01-12 01:14:42 +0600

    ubthub publish

--- ErkinStudy.Web/Controllers/UbtHubController.cs
+++ ErkinStudy.Web/Controllers/UbtHubController.cs
@@ -4,18 +4,16 @@ using System.Threading.Tasks;
 using ErkinStudy.Infrastructure.Services;
 using ErkinStudy.Web.Models.UbtHub;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace ErkinStudy.Web.Controllers
 {
     public class UbtHubController : Controller
     {
         private readonly SpecialtyService _specialtyService;
-        private readonly ILogger<UbtHubController> _logger;
-        public UbtHubController(SpecialtyService specialtyService, ILogger<UbtHubController> logger)
+
+        public UbtHubController(SpecialtyService specialtyService)
         {
             _specialtyService = specialtyService;
-            _logger = logger;
         }
 
         public  IActionResult Index()
